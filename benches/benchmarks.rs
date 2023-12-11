a6c88deb62cf9ad797360dc250ff562369a78001
Alexander Korn <dev@lxnd.me>
2023-12-11 18:38:13 +0100

    Add benchmark input

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -2,6 +2,7 @@ mod benchmarks_dp;
 mod benchmarks_walkers;
 
 use crate::benchmarks_dp::benchmarks_dp;
+use crate::benchmarks_walkers::benchmarks_walkers;
 use criterion::{criterion_group, criterion_main};
 
 criterion_main!(benchmarks_dp);
